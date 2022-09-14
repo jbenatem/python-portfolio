0d3684deab3597e53c2bab9be0f9e5a7a83d6c7e
jbenatem <jbenatem@gmail.com>
2022-09-14 18:50:50 -0500

    graphic interface to save a record in db

--- tkinter-sample/main.py
+++ tkinter-sample/main.py
@@ -1,29 +1,39 @@
+from tkinter import *
 from util.userDAO import UserDAO
 from util.entities.user import User
 
-if __name__ == "__main__":
-    userDAO = UserDAO()
-    
-    # Insert User
-    #user1 = User("Steve","Rogers",28,"capamerica@test.com","999999999")
-    user1 = User("Bruce","Banner",30,"hulk@test.com","919191919")
-    userDAO.insertUser(user1)
-    
-    # List all users
-    users = userDAO.listUsers()
-    i = 1
-    for user in users:
-        print("--------------------------user % s--------------------------" % (i))
-        print(user.showInfo())
-        i+=1
-    
-    # List a specific user
-    user = userDAO.listUser("2")
-    print(user.showInfo())
-    
-    # Delete an user
-    userDAO.deleteUser("3")
-    
-    # Update an user
-    userTest = User("Thor","Odinson",1000,"thor@test.com","912913914")
-    userDAO.editUser(userTest, "4")
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1=Label(win, text='Firstname')
+        self.lbl2=Label(win, text='Lastname')
+        self.lbl3=Label(win, text='Age')
+        self.lbl4=Label(win, text='Email')
+        self.lbl5=Label(win, text='Phone')
+        self.t1=Entry(bd=3)
+        self.t2=Entry()
+        self.t3=Entry()
+        self.t4=Entry()
+        self.t5=Entry()
+        self.btn1 = Button(win, text='Submit')
+        self.lbl1.place(x=100, y=50)
+        self.t1.place(x=200, y=50)
+        self.lbl2.place(x=100, y=100)
+        self.t2.place(x=200, y=100)
+        self.lbl3.place(x=100, y=150)
+        self.t3.place(x=200, y=150)
+        self.lbl4.place(x=100, y=200)
+        self.t4.place(x=200, y=200)
+        self.lbl5.place(x=100, y=250)
+        self.t5.place(x=200, y=250)
+        self.b1=Button(win, text='Add', command=self.add)
+        self.b1.place(x=100, y=300)        
+    def add(self):
+        user = User(self.t1.get(),self.t2.get(),int(self.t3.get()),self.t4.get(),self.t5.get())
+        userDAO = UserDAO()
+        userDAO.insertUser(user)
+
+window=Tk()
+mywin=MyWindow(window)
+window.title('Hello Python')
+window.geometry("400x400+10+10")
+window.mainloop()
